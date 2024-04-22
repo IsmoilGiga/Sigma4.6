bfc07e74344b77b8c988cbb8dbf3435295a94172
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-04-22 11:50:22 +0500

    Sigma4.6

---
+++ index.js
@@ -0,0 +1,23 @@
+let son1 = Number(prompt("Birinchi sonni kiriting "));
+let son2 = Number(prompt("Ikkinchi sonni kiriting "));
+let amal = String(prompt("Amalni kiriting (+, -, *, /): "));
+
+if (amal === "+") {
+    let yigindi = son1 + son2;
+    alert(son1 + " + " + son2 + " = " + yigindi);
+}
+
+if (amal === "-") {
+    let yigindi = son1 - son2;
+    alert(son1 + " - " + son2 + " = " + yigindi);
+}
+
+if (amal === "*") {
+    let yigindi = son1 - son2;
+    alert(son1 + " * " + son2 + " = " + yigindi);
+}
+
+if (amal === "/") {
+    let yigindi = son1 / son2;
+    alert(son1 + " / " + son2 + " = " + yigindi);
+}
